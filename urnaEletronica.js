22c6b09ee4d6e76bf03f881f2d6f406a37f06ee2
barbaraUchoa <barbarahelenauchoa@gmail.com>
2023-10-31 11:58:13 -0300

    feat: exercício 9 arrumado

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -1,206 +1,223 @@
-function data (){
+function data() {
     const data = new Date().toLocaleString();
     return data
 }
 function verificarIntegridadeUrna() {
 
-    fetch ('urnaEletronica.js')
-    .then(conteudo => {conteudo.text()})
-    .then(conteudo => CryptoJS.SHA256(conteudo).toString())
-    .then(hashUrnaAtual => {
-        fetch('./hashVerificado')
-        .then(conteudo =>conteudo.text())
-        .then(hashVerificado => {
+    fetch('urnaEletronica.js')
+        .then(conteudo => { conteudo.text() })
+        .then(conteudo => CryptoJS.SHA256(conteudo).toString())
+        .then(hashUrnaAtual => {
+            fetch('./hashVerificado')
+                .then(conteudo => conteudo.text())
+                .then(hashVerificado => {
 
 
-        if(hashUrnaAtual === hashVerificado){
-            console.log('Hash verificado,urna integra')
+                    if (hashUrnaAtual === hashVerificado) {
+                        console.log('Hash verificado,urna integra')
 
-            } else {  
+                    } else {
 
-                console.log('Hashes Diferentes, URNA ADULTERADA!');
-                console.log(`Hash esperado: ${hashVerificado}`);
-                console.log(`Hash da Urna: ${hashUrnaAtual}`);
-            }
-            
+                        console.log('Hashes Diferentes, URNA ADULTERADA!');
+                        console.log(`Hash esperado: ${hashVerificado}`);
+                        console.log(`Hash da Urna: ${hashUrnaAtual}`);
+                    }
+
+                }
+                )
         }
-    )}
-)}
+        )
+}
 
 
 function urnaEletronica() {
-    
-    document.getElementById('nomeDosCandidatos').innerHTML=''
+
+    document.getElementById('nomeDosCandidatos').innerHTML = ''
 
 
     let candidatos = [
-    
-        [56,'Enéas'],
-        [45,'Pancadinha'],
-        [22,'Maria do Bairro'],
-        [13,'Mazzaropi'],
-        [50,'Bico da Gaita'],
-        ['00','Branco']
-        ]
-        
-    for (i = 0; i < candidatos.length; i++){
-        
-   
-        document.getElementById('nomeDosCandidatos').innerHTML += `<p>${candidatos[i][0]} - ${candidatos[i][1]} </p>` 
-    
+
+        [56, 'Enéas'],
+        [45, 'Pancadinha'],
+        [22, 'Maria do Bairro'],
+        [13, 'Mazzaropi'],
+        [50, 'Bico da Gaita'],
+        ['00', 'Branco']
+    ]
+
+    for (i = 0; i < candidatos.length; i++) {
+
+
+        document.getElementById('nomeDosCandidatos').innerHTML += `<p>${candidatos[i][0]} - ${candidatos[i][1]} </p>`
+
     }
-    
-    
-    
+
+
+
     console.clear();
-    let senhaMesario = prompt ('Digite uma senha de 6 dígitos (123456)');
+    let senhaMesario = prompt('Digite uma senha de 6 dígitos (123456)');
 
 
- // declaração das variáveis
+    // declaração das variáveis
 
- let contador = 0;
-let nome1
-let nome2
-let nome3
- let Opção
- let candidato1 = 0;
- let candidato2 = 0;
- let candidato3 = 0;
- let votobranco = 0;
- let votonulo = 0;
- let encerrarVotação = "N";
- let datainicio = data();
- let datafinal = data();
- 
- 
- 
- 
- do {
-     
-     console.log(' Configuração da urna');
-     opcao = parseInt(prompt("inicio da votação"));
-     
-  
-     console.log('Repetição');
-     contador++;
-     
-     if (opcao === 1) {
-         if (confirm('Confirma seu voto no candidato?:  ' + candidatos[0][0] +   ' - ' + candidatos[0][1] ))
-         {
-             console.log('Opção  de voto é o candidato',candidatos[0][1]);
-             candidato1++;
-             
+    let contador = 0;
+    let nome1 = candidatos[0][1];
+    let nome2 = candidatos[1][1];
+    let nome3 = candidatos[2][1];
+    let nome4 = candidatos[3][1];
+    let nome5 = candidatos[4][1];
+    let branco = candidatos[5][1];
+    let opcao
+    let candidato1 = 0;
+    let candidato2 = 0;
+    let candidato3 = 0;
+    let candidato4 = 0;
+    let candidato5 = 0;
+    let votobranco = 0;
+    let encerrarVotação = "N";
+    let datainicio = data();
+    
+    
+    
+    
+    do {
+        
+        //  console.log(' Configuração da urna');
+        opcao = parseInt(prompt("inicio da votação"));
+        
+        
+        console.log('Repetição');
+        contador++;
+        
+        if (opcao === candidatos[0][0]) {
+            if (confirm('Confirma seu voto no candidato?:  ' + nome1 + ' - ' + candidatos[0][0])) {
+                console.log('Opção  de voto é o candidato', nome1);
+                candidato1++;
+                
             }
             
-        } else if (opcao === 2) {
-            if (confirm('Confirma seu voto no candidato?: ' + nome2))
-            
-            {
+        } else if (opcao ===  candidatos[1][0]) {
+            if (confirm('Confirma seu voto no candidato?: ' + nome2 + candidatos[1][0])) {
                 console.log('Opção de voto é o candidato', nome2);
                 
                 candidato2++;
             }
-            
-        } else if (opcao === 3) {
-            console.log('Opção de voto é o candidato',nome3);
-            
-            if ( confirm('Confirma seu voto no candidato?: ' + nome3))
-            
-            {
-                console.log('Opção de voto é o candidato',nome3);
+
+        } else if (opcao === candidatos[2][0]) {
+        
+            if (confirm('Confirma seu voto no candidato?: ' + nome3 + candidatos[2][0])) {
+                console.log('Opção de voto é o candidato', nome3);
                 
-                candidato3 ++;
+                candidato3++;
             }
             
-        } else if (opcao == 4) {
-            if (confirm('Confirma seu no em Branco? '))
-            {
+            
+        } else if (opcao === candidatos[3][0]) {
+            
+            if (confirm('Confirma seu voto no candidato?: ' + nome4 + candidatos[3][0])) {
+                console.log('Opção de voto é o candidato', nome4);
+                
+                candidato4++;
+                
+                
+            }
+        } else if (opcao === candidatos[4][0]) {
+        
+            if (confirm('Confirma seu voto no candidato?: ' + nome5 + candidatos[4][0])) {
+                console.log('Opção de voto é o candidato', nome5);
+                
+                candidato5++;
+            }
+            
+        } else if (opcao == candidatos[5][0]) {
+            if (confirm('Confirma seu no em Branco? ' + candidatos[5][0])) {
                 
                 console.log('Opção  do voto é  em Branco');
+
+                branco++;
                 
-                votobranco++;
             }
             
-        } else if (opcao == 0 ){
+        } else if (opcao == 0) {
             return;
             
         } else if (opcao == senhaMesario) {
             encerrarVotação = (prompt(' Deseja REALMENTE ENCERRAR ESSA VOTAÇÃO? Digite S para encerrar e N para continuar'))
-            if(encerrarVotação == 'N'){
+            if (encerrarVotação == 'N') {
                 
             }
             
-            if (encerrarVotação === 'S'){
-                console.log('Votação encerrada com Sucesso!')
+            if (encerrarVotação === 'S') {
+                console.log('Votação encerrada com Sucesso!') 
             }
             
             
             
-            
-            
-            
-        }else {
-            opcaoVtonULO = confirm('ATENÇÃO: SEU VOTO SERA ANULADO. DESEJA PROCESSEGUIR?');
-            if (opcaoVtonULO){
-                votonulo++
-            }
         }
         
-    }while (encerrarVotação !== "s");
-        
+    } while (encerrarVotação !== "s");
+    
+    let datafinal = data();
     //se ouver votaçao
-    
+
     console.log('=====================Boletim da Urna====================')
-    
-    
-    let totalDeVotos = candidato1 + candidato2 + candidato3 + votobranco + votonulo;
-    console.clear ();
+
+
+    let totalDeVotos = candidato1 + candidato2 + candidato3 + candidato4 + candidato5 + votobranco;
+    console.clear();
     console.log('Data e horário do inicío  da votação' + datainicio);
-    console.log('Este é o total de votos para o candidato(a) ', nome1,':', candidato1 );
-    console.log('Este é o total de votos para o candidato ', nome2,':',candidato2);
-    console.log('Este é o total de votos para o candidato ', nome3,':',candidato3);
+    console.log('Este é o total de votos para o candidato(a) ', nome1, ':', candidato1);
+    console.log('Este é o total de votos para o candidato ', nome2, ':', candidato2);
+    console.log('Este é o total de votos para o candidato ', nome3, ':', candidato3);
+    console.log('Este é o total de votos para o candidato ', nome4, ':', candidato4);
+    console.log('Este é o total de votos para o candidato ', nome5, ':', candidato5);
     console.log('Este é o total de votos para o Voto Branco', votobranco);
-    console.log('Este é o total de votos para o Voto Nulo', votonulo);
-    console.log('Este é o Total da votação',totalDeVotos )
-    
-         contador = contador - 1;
-         
-         if (candidato1 > candidato2 && candidato1 > candidato3) {
-        console.log('O ganhador é  ',nome1);
-        console.log('Total de Votos válidos mais Brancos',votobranco + candidato1);
-        console.log('Porcentagem do ganhador', (candidato1 + votobranco) / totalDeVotos * 100)
-    }else if (candidato2 > candidato1 && candidato2 > candidato3){
-        console.log('O ganhador é  ',nome2);
-        console.log('Total de Votos válidos mais Brancos',votobranco + candidato2);
-    }else if (candidato3 > candidato1 && candidato3 > candidato2){
-        console.log('O ganhador é  ',nome3);
-        console.log('Total de Votos válidos mais Brancos',votobranco + candidato3);
-    }else {
-        console.log (' não ouve ganhador nesta urna (Empate entre 2 ou mais candidaos)');
+
+    console.log('Este é o Total da votação', totalDeVotos)
+
+    contador = contador - 1;
+
+    if (candidato1 > candidato2 && candidato1 > candidato3 && candidato1 > candidato4 && candidato1 > candidato5) {
+        console.log('O ganhador é  ', nome1);
+        console.log('Total de Votos válidos mais Brancos', branco + candidato1);
+        console.log('Porcentagem do ganhador', (candidato1 + branco) / totalDeVotos * 100)
+    } else if (candidato2 > candidato1 && candidato2 > candidato3 && candidato2 > candidato4 && candidato2 > candidato5) {
+        console.log('O ganhador é  ', nome2);
+        console.log('Total de Votos válidos mais Brancos', branco + candidato2);
+    } else if (candidato3 > candidato1 && candidato3 > candidato2 && candidato3 > candidato4 && candidato3 > candidato5) {
+        console.log('O ganhador é  ', nome3);
+        console.log('Total de Votos válidos mais Brancos', branco + candidato3);
+    } else if (candidato4 > candidato1 && candidato4 > candidato2 && candidato4 > candidato3 && candidato4 > candidato5) {
+        console.log('O ganhador é  ', nome4);
+        console.log('Total de Votos válidos mais Brancos', branco + candidato3);
+    } else if (candidato5 > candidato1 && candidato5 > candidato2 && candidato5 > candidato4 && candidato5 > candidato3) {
+        console.log('O ganhador é  ', nome3);
+        console.log('Total de Votos válidos mais Brancos', branco + candidato3);
+    } else {
+        console.log(' não ouve ganhador nesta urna (Empate entre 2 ou mais candidaos)');
         empate = true
-        
-        
     }
-    
-    let porcentagemCandidato1= candidato1 / totalDeVotos * 100
-    console.log( nome1, 'votos', porcentagemCandidato1.toFixed(2), '%')
-    
-    let porcentagemCandidato2= candidato2 / totalDeVotos * 100
-    console.log( nome2, 'votos', porcentagemCandidato2.toFixed(2) ,'%')
 
-    let porcentagemCandidato3= candidato3 / totalDeVotos * 100
-    console.log( nome3, 'votos', porcentagemCandidato3.toFixed(2), '%')
-    
-    let porcentagemVotosBrancos= votobranco / totalDeVotos * 100
-    console.log( 'Votos em Branco ', porcentagemVotosBrancos.toFixed(2),'%')
+    let porcentagemCandidato1 = candidato1 / totalDeVotos * 100
+    console.log(nome1, 'votos', porcentagemCandidato1.toFixed(2), '%')
 
-    let porcentagemVotosNulos= votonulo / totalDeVotos * 100
-    console.log('Votos Nulo', porcentagemVotosNulos.toFixed(2), '%')
-    console.log = datafinal(' Data e horário do término da votação ' + data());
+    let porcentagemCandidato2 = candidato2 / totalDeVotos * 100
+    console.log(nome2, 'votos', porcentagemCandidato2.toFixed(2), '%')
+
+    let porcentagemCandidato3 = candidato3 / totalDeVotos * 100
+    console.log(nome3, 'votos', porcentagemCandidato3.toFixed(2), '%')
+
+    let porcentagemCandidato4 = candidato4 / totalDeVotos * 100
+    console.log(nome4, 'votos', porcentagemCandidato4.toFixed(2), '%')
+
+    let porcentagemCandidato5 = candidato5 / totalDeVotos * 100
+    console.log(nome5, 'votos', porcentagemCandidato5.toFixed(2), '%')
+
+    let porcentagemVotosBrancos = branco / totalDeVotos * 100
+    console.log('Votos em Branco ', porcentagemVotosBrancos.toFixed(2), '%')
+
+    console.log(' Data e horário do término da votação'+ datafinal);
     verificarIntegridadeUrna();
-    console.log (' Encerrada a Votação');
-    
-    
-    
-    
+    console.log(' Encerrada a Votação');
+
 }  
